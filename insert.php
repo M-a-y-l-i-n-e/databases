d2bdca8f7473f82a6d422d9a596c3611671b1238
M-a-y-l-i-n-e <58560768+M-a-y-l-i-n-e@users.noreply.github.com>
2019-12-18 09:23:41 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,7 +9,7 @@ $conn = new mysqli ($servername,$username,$password,$dbname);
 if($conn->connect_error){
 	die("connection failed" . $conn->connect_error);
 }
-$sql = "INSERT INTO stagiaire (nom, prenom) VALUES ('iks', 'igrek')";
+$sql = "INSERT INTO stagiaire (nom, prenom) VALUES (\"$_POST['nom']\",\"$_POST['prenom']\")";
 $conn->query($sql);
 $conn->close();
 ?>
